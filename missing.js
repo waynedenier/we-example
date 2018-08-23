dcf946d9cda4030f4c4b0785ffd014e4d46dbfcf
Wayne Denier II <wayne.denier@gmail.com>
2018-08-23 10:39:09 -0400

    Removed old approach.

--- missing.js
+++ missing.js
@@ -11,19 +11,3 @@ console.log(missing([1,4,3]));
 console.log(missing([2,3,4]));
 console.log(missing([5,1,4,2]));
 console.log(missing([1,2,3,4]));
-
-// NOTE: This approach did not work
-// var missing = function(subject){
-//     var projectedTotal = 0, actualTotal = 0;
-//     for (let i = 0; i < subject.length; i++) {
-//         projectedTotal += i + 1;
-//         actualTotal += subject[i];
-//     }
-
-//     console.log('a: ' + actualTotal);
-//     console.log('b: ' + projectedTotal);
-
-//     var missingValue = actualTotal - projectedTotal;
-//     if(missingValue === 0) return undefined;
-//     return missingValue;
-// };
